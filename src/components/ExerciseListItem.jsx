e5a30c71fde2815edb50bf1e70a060d30dfd93d6
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2024-02-05 20:23:43 +0200

    capitalize

--- src/components/ExerciseListItem.jsx
+++ src/components/ExerciseListItem.jsx
@@ -1,33 +1,32 @@
-import {Text, View , StyleSheet} from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function ExerciseListItem({ item }) {
   return (
     <View style={styles.exerciseContainer}>
       <Text style={styles.exerciseName}>{item.name}</Text>
       <Text style={styles.exerciseSubtitle}>
-        <Text style={styles.subValue}>{item.muscle}</Text> | <Text style={styles.subValue}>{item.equipment}</Text>
+        <Text style={styles.subValue}>{item.muscle}</Text> |{' '}
+        <Text style={styles.subValue}>{item.equipment}</Text>
       </Text>
     </View>
   );
 }
 
-
 const styles = StyleSheet.create({
-    exerciseContainer: {
-      backgroundColor: '#fff',
-      padding: 10,
-      borderRadius: 10,
-      gap: 5,
-    },
-    exerciseName: {
-      fontSize: 20,
-      fontWeight: '500',
-    },
-    exerciseSubtitle: {
-      color: 'dimgray',
-    },
-    subValue:{
-      textTransform:'capitalize',
-    },
-  });
-  
+  exerciseContainer: {
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 10,
+    gap: 5,
+  },
+  exerciseName: {
+    fontSize: 20,
+    fontWeight: '500',
+  },
+  exerciseSubtitle: {
+    color: 'dimgray',
+  },
+  subValue: {
+    textTransform: 'capitalize',
+  },
+});
